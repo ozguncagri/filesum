2c7b310206096371551bf7193a298256b99e33be
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-03-17 02:14:13 +0300

    changed execution logger

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
 
 	"github.com/spf13/cobra"
 )
@@ -35,7 +34,6 @@ func main() {
 	})
 
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
