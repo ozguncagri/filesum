fe78efab08dfeda25ad32d830d423abe8ca1499a
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-03-17 02:04:51 +0300

    added cobra template for command

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "github.com/spf13/cobra"
 
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "filesum",
+		Short: "File integrity calculator",
+		Long:  "Filesum calculates integrity signatures of file with different algorithms",
+	}
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:     "md5",
+		Short:   "Calculate MD5 signature of file",
+		Long:    "Calculates MD5 signature of file and prints to stdout",
+		Example: "filesum md5 <filePath>",
+		Run: func(cmd *cobra.Command, args []string) {
+			//md5 calculation
+		},
+	})
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:     "sha1",
+		Short:   "Calculate SHA1 signature of file",
+		Long:    "Calculates SHA1 signature of file and prints to stdout",
+		Example: "filesum sha1 <filePath>",
+		Run: func(cmd *cobra.Command, args []string) {
+			//sha1 calculation
+		},
+	})
 }
